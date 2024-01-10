72cd93bd40eb11a38e8f68a87c2ec204b5fb3214
M4r1ey <mwendamarley13@gmail.com>
2024-01-10 12:52:28 +0300

    2nd

--- src/assets/data/carData.js
+++ src/assets/data/carData.js
@@ -1,8 +1,6 @@
 // import all images from assets/images directory
 import img01 from "../all-images/cars-img/img01.jpg";
-import img02 from "../all-images/cars-img/offer-toyota.png";
 import img03 from "../all-images/cars-img/bmw-offer.png";
-import img04 from "../all-images/cars-img/img01.jpg";
 import img05 from "../all-images/cars-img/offer-toyota.png";
 import img06 from "../all-images/cars-img/mercedes-offer.png";
 import img07 from "../all-images/cars-img/toyota-offer-2.png";
